bbb19ddcb4f1cd489a0a1f7c669611b5e5b9ca68
zz0807 <zz0807@126.com>
2017-03-21 16:46:07 +0800

    test

--- extract.py
+++ extract.py
@@ -1,7 +1,9 @@
 from bow import BagOfWords
 from bow import WordFilters
+from sklearn.ensemble import RandomForestClassifier
 
-file = open('train_1000.label','r')
+
+file = open('test.label','r')
 label = [] # label for every sentence
 total_word = []  # to store the word that appeared in the file in list
 sentence_total = [] # to store every sentence in file in list
@@ -48,14 +50,26 @@ for sentence in sentence_total:
                 temp[temp_key] = sentence[sentence_key]
     sample_list.append(temp)
 
-for key in sample_list[0]:
-    if sample_list[0][key] == 1:
-        print key
+data = []
+data_sentence = []
+# convert dict value into list
+for i in sample_list:
+    data_sentence = list(i.values())
+    data.append(data_sentence)
 
-
-
-
-
-
-
-
+rf = RandomForestClassifier()
+rf.fit(data,label)
+RandomForestClassifier(bootstrap=True, class_weight=None, criterion='gini',
+            max_depth=None, max_features='auto', max_leaf_nodes=None,
+            min_samples_leaf=1, min_samples_split=2,
+            min_weight_fraction_leaf=0.0, n_estimators=10, n_jobs=1,
+            oob_score=False, random_state=None, verbose=0,
+            warm_start=False)
+test_sample = []
+test_sample.append(data[4])
+print rf.predict_proba(test_sample)
+# DESC ENTY HUM LOC
+s = set(label)
+l = list(s)
+l.sort()
+print l
